c2e248f2e01a17d19802d6e3cb1f4e158a8427d6
Patryk Piotrowski <patryk.piotrowski@lucidrealitylabs.com>
2024-08-10 22:58:12 +0200

    Init commit, created basic asset loader helper classes

---
+++ Source/AssetLoader/AssetLoader.Build.cs
@@ -0,0 +1,29 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AssetLoader : ModuleRules
+{
+	public AssetLoader(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"Engine"
+			}
+		);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore"
+			}
+		);
+	}
+}
---
+++ Source/AssetLoader/Public/AssetLoaderHelper.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include "Engine/AssetManager.h"
+
+class FAssetLoaderHelper
+{
+public:
+	// Template function for loading a soft object asynchronously
+	template <typename TObjectType>
+	static TFuture<TObjectType*> LoadAsync(TSoftObjectPtr<TObjectType> SoftObjectPointer);
+
+	// Template function for loading a soft class asynchronously
+	template <typename TObjectType>
+	static TFuture<TSubclassOf<TObjectType>> LoadAsync(TSoftClassPtr<TObjectType> SoftClassPointer);
+};
+
+// Implementation of the LoadAsync function for soft object pointers
+template <typename TObjectType>
+TFuture<TObjectType*> FAssetLoaderHelper::LoadAsync(TSoftObjectPtr<TObjectType> SoftObjectPointer)
+{
+	TSharedPtr<TPromise<TObjectType*>> Promise = MakeShared<TPromise<TObjectType*>>();
+	TFuture<TObjectType*> Future = Promise->GetFuture();
+
+	FStreamableManager& StreamableManager = UAssetManager::GetStreamableManager();
+
+	StreamableManager.RequestAsyncLoad(SoftObjectPointer.ToSoftObjectPath(), FStreamableDelegate::CreateLambda(
+		                                   [SoftObjectPointer, Promise]() mutable
+		                                   {
+			                                   TObjectType* LoadedObject = SoftObjectPointer.Get();
+			                                   Promise->SetValue(LoadedObject);
+		                                   }));
+
+	return Future;
+}
+
+// Implementation of the LoadAsync function for soft class pointers
+template <typename TObjectType>
+TFuture<TSubclassOf<TObjectType>> FAssetLoaderHelper::LoadAsync(TSoftClassPtr<TObjectType> SoftClassPointer)
+{
+	TSharedPtr<TPromise<TSubclassOf<TObjectType>>> Promise = MakeShared<TPromise<TSubclassOf<TObjectType>>>();
+	TFuture<TSubclassOf<TObjectType>> Future = Promise->GetFuture();
+
+	FStreamableManager& StreamableManager = UAssetManager::GetStreamableManager();
+
+	StreamableManager.RequestAsyncLoad(SoftClassPointer.ToSoftObjectPath(), FStreamableDelegate::CreateLambda(
+		                                   [SoftClassPointer, Promise]() mutable
+		                                   {
+			                                   TSubclassOf<TObjectType> LoadedObject = SoftClassPointer.Get();
+			                                   Promise->SetValue(LoadedObject);
+		                                   }));
+
+	return Future;
+}
